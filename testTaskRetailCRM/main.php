afbc13c3aadcb12f1f6e34568cc0122ff596b044
UshurbakievStep <ushurbakiev@itstep.kz>
2021-10-26 09:34:31 +0600

    added version 1.2

--- testTaskRetailCRM/main.php
+++ testTaskRetailCRM/main.php
@@ -12,11 +12,16 @@ use RetailCrm\Api\Factory\SimpleClientFactory;
 use RetailCrm\Api\Interfaces\ApiExceptionInterface;
 use RetailCrm\Api\Interfaces\ClientExceptionInterface;
 use RetailCrm\Api\Model\Entity\Customers\Customer;
+use RetailCrm\Api\Model\Entity\Orders\Items\Offer;
+use RetailCrm\Api\Model\Entity\Orders\Items\OrderProduct;
+use RetailCrm\Api\Model\Entity\Orders\Items\PriceType;
+use RetailCrm\Api\Model\Entity\Orders\Items\Unit;
 use RetailCrm\Api\Model\Entity\Orders\Order;
+use RetailCrm\Api\Model\Entity\Orders\Payment;
 use RetailCrm\Api\Model\Request\Customers\CustomersCreateRequest;
 use RetailCrm\Api\Model\Request\Orders\OrdersCreateRequest;
 
-require $_SERVER['DOCUMENT_ROOT'].'\RetailCrmApiTestCase\testTaskRetailCRM\vendor\autoload.php';
+require $_SERVER['DOCUMENT_ROOT'] . '\RetailCrmApiTestCase\testTaskRetailCRM\vendor\autoload.php';
 
 class RetailCRMApi
 {
@@ -40,36 +45,60 @@ class RetailCRMApi
     {
 
         $request = new OrdersCreateRequest();
-        $request->order = new Order();
+        $order = new Order();
+        $offer = new Offer();
+        $items = new OrderProduct();
+        $payment = new Payment();
 
-        $request->site = 'test'; //магазин
-        $request->order->orderType = "fzik"; // тип заказа
-        $request->order->orderMethod = "test"; // способ оформления
-        $request->order->number = "19032000"; // номер заказа
-        $request->order->firstName = "Davlatbek"; // Фамилия
-        $request->order->lastName = "Ushurbakiyev"; // Имя
-        $request->order->customFields['prim'] = "тестовое задание"; // примичание
-        $request->order->payments[0]->comment = "https://github.com/UshurbakiyevDavlat/RetailCrmApiTestCase"; // коммент
-        $request->order->items[0]->offer->article = "AZ105R"; // артикул товара
-        $request->order->company->brand = "Azalita"; // бренд
+        //оплата
+        $payment->type = 'bank-card';
+        $payment->status = 'paid';
+        $payment->amount = 1000;
+        $payment->paidAt = new DateTime();
+
+        //характеристика товара
+        $offer->name = 'AZ105R Azalita';
+        $offer->displayName = 'Маникюрный набор AZ105R Azalita';
+        $offer->xmlId = 'tGunLo27jlPGmbA8BrHxY2';
+        $offer->article = 'AZ105R';
+        $offer->unit = new Unit('AZ105R', 'Azalita', 'Azalita');
+
+        // товар
+        $items->offer = $offer;
+        $items->priceType = new PriceType('base');
+        $items->quantity = 1;
+        $items->purchasePrice = 60;
+
+        // заказ
+        $order->items = [$items];
+        $order->payments = [$payment];
+        $order->status = "trouble";
+        $order->orderType = "fzik"; // тип заказа
+        $order->orderMethod = "test";// способ оформления
+        $order->number = "19032000"; // номер заказа
+        $order->firstName = "Davlatbek"; // Фамилия
+        $order->lastName = "Ushurbakiyev"; // Имя
+        $order->customFields = [
+            "prim" => "тестовое задание"
+        ]; // примечание
+        $order->managerComment = "https://github.com/UshurbakiyevDavlat/RetailCrmApiTestCase"; // коммент
 
 
-        $client = self::connection();
+        $request->order = $order; // инициализация товара
+        $request->site = "test"; // магазин
+
+        $client = self::connection(); // запуск соединения
         $response = [];
 
         try {
-            $response = $client->orders->create($request);
-        } catch (AccountDoesNotExistException $e) {
-        } catch (ApiErrorException $e) {
-        } catch (MissingCredentialsException $e) {
-        } catch (MissingParameterException $e) {
-        } catch (ValidationException $e) {
-        } catch (HandlerException $e) {
-        } catch (HttpClientException $e) {
-        } catch (ApiExceptionInterface $e) {
-        } catch (ClientExceptionInterface $e) {
+            $response = $client->orders->create($request); // создание запроса на создание товара
+        } catch (AccountDoesNotExistException | ApiErrorException | MissingCredentialsException | MissingParameterException | ValidationException | HandlerException | HttpClientException | ApiExceptionInterface | ClientExceptionInterface $e) {
         }
-        echo "Status" . $response->success;
+        printf(
+            'Created order id = %d with the following data: %s',
+            $response->id, // вывод
+            print_r($response->order, true)
+        );
     }
 
     /**
@@ -87,7 +116,6 @@ $callManager = new RetailCRMApi();
 
 try {
     $callManager->main();
-} catch (BuilderException $e) {
-} catch (ClientExceptionInterface $e) {
+} catch (BuilderException | ClientExceptionInterface $e) {
 }
 
